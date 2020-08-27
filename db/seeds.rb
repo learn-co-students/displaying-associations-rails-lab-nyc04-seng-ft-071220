7f060cede801a9de2eb7ee50e809c63e48b5e865
Chenyun Zhang <morningchenyun@gmail.com>
2020-08-27 11:35:22 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Song.destroy_all
+Artist.reset_pk_sequence
+Song.reset_pk_sequence
+
+artist_1 = Artist.create!(name: "Billie Eilish")
+artist_1.songs.create!(title: "bad guy")
+artist_1.songs.create!(title: "I love you")
+
+
+artist_2 = Artist.create!(name: "Adele")
+artist_2.songs.create!(title: "Hello")
+artist_2.songs.create!(title: "someone like you")
+
+puts "done"
