cc1a5c27d3482afd5160bbc6d63d965dafcc7781
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-08-27 22:58:31 -0400

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Song.destroy_all
+Artist.reset_pk_sequence
+Song.reset_pk_sequence
+
+blake = Artist.create!(name: "Blake")
+blake.songs.create!(title: "I Feel down")
+blake.songs.create!(title: "The World on fire")
+blake.songs.create!(title: "Swimming with the fish")
+
+larry = Artist.create!(name: "Larry")
+larry.songs.create!(title: "Hop Step Jump")
